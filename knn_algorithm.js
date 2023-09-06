81f70f6eb6e4ca5a6a4947575021535768d31dcc
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-06 14:04:06 +0200

    Update knn_algorithm.js

--- knn_algorithm.js
+++ knn_algorithm.js
@@ -1,10 +1,26 @@
-
 class knn_algo{
-	let k = 5;
+	constructor(k = 5) {
+		this.k = 5;
+	}
 	calc_euclidean_distance(x1,x2) {
-    
-  }
- predict(x_train,x_test,y_train){
- 	
- }
+		Math.sqrt((x1-x2).reduce((a, b) => a + b * b, 0))
+	}
+	predict(x_train,x_test,y_train){
+		
+	}
+	_predict(x){
+		let distances = self.X_train.map(x_train => euclidean_distance(x, x_train));
+		let indices = Array.from(distance.keys());
+		indices.sort((a, b) => distance[a] - distance[b]);
+		let result = indices.slice(0, self.k);
+
+		let indices = Array.from(distance.keys());
+		indices.sort((a, b) => distance[a] - distance[b]);
+		let result = indices.slice(0, self.k);
+
+
+	}
+	fit(X,y){
+		 
+	}
 }
